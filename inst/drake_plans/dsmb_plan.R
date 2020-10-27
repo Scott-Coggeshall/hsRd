743161344065f23ed4b3808e6ee844df48d76b16
Coggeshall, Scott Samuel (Puget Sound) <Scott.Coggeshall@va.gov>
2020-10-26 23:11:02 -0700

    modify dsmb_plan to add consort_table and randomization_figure targets

--- inst/drake_plans/dsmb_plan.R
+++ inst/drake_plans/dsmb_plan.R
@@ -1,8 +1,17 @@
 dsmb_plan <- drake_plan(
 
-  dsmb_data = read.csv(file_in("data/dsmb.csv")), #replace with your source data
 
-  enrollment_summary = summarize_enrollment(dsmb_data),
+  enrollment_data = read.csv(file_in(file.path("data", "enrollment.csv"))), #replace with your source data
+
+  randomization_data = read.csv(file_in(file.path("data", "randomization.csv"))),
+
+  followup_data = read.csv(file_in(file.path("data", "followup.csv"))),
+
+  status_data = rbind(enrollment_data, randomization_data, followup_data),
+
+  consort_table = build_consort_table(status_data),
+
+  randomization_figure = plot_randomization(randomization_data),
 
   adverse_event_summary = summarize_adverse_event(dsmb_data),
 
